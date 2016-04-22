abcdd0b70ee795a94cd65fda594f2d9fcce3cfd7
ShadowLeaf1 <piyushnagpal010@gmail.com>
2016-04-22 18:36:32 +0530

    Added cm.mk

---
+++ cm.mk
@@ -0,0 +1,11 @@
+$(call inherit-product, device/lenovo/aio_row/device_K50a40.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := K50a40
+PRODUCT_NAME := cm_K50a40
+PRODUCT_BRAND := Lenovo
+PRODUCT_MODEL := Lenovo K50a40 
+PRODUCT_MANUFACTURER := Lenovo
