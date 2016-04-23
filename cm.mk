6e883f4ee22c1d8bd6728b1530f3e6c2200972ae
Piyush Nagpal <piyushnagpal010@gmail.com>
2016-04-23 10:24:13 +0530

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -4,8 +4,8 @@ $(call inherit-product, device/lenovo/aio_row/device_K50a40.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := K50a40
-PRODUCT_NAME := cm_K50a40
-PRODUCT_BRAND := Lenovo
-PRODUCT_MODEL := Lenovo K50a40 
-PRODUCT_MANUFACTURER := Lenovo
+PRODUCT_DEVICE                                := K50a40
+PRODUCT_NAME                                  := cm_K50a40
+PRODUCT_BRAND                                 := Lenovo
+PRODUCT_MODEL                                 := Lenovo K50a40 
+PRODUCT_MANUFACTURER                          := Lenovo
