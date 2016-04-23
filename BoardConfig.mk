bc9c156b891006e4714a74b3d6048c352c11d164
Piyush Nagpal <piyushnagpal010@gmail.com>
2016-04-23 10:21:41 +0530

    Cleaned Up.....

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,150 +1,148 @@
 # mt6795 platform boardconfig
-LOCAL_PATH := device/lenovo/K50a40
+LOCAL_PATH                                    := device/lenovo/K50a40
 -include vendor/lenovo/K50a40/BoardConfigVendor.mk
 
-TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
+TARGET_SPECIFIC_HEADER_PATH                   := $(LOCAL_PATH)/include
 
-USE_CAMERA_STUB := true
-TARGET_PROVIDES_INIT_RC := true
+USE_CAMERA_STUB                               := true
+TARGET_PROVIDES_INIT_RC                       := true
 # inherit from the proprietary version
 
 # Platform
-ARCH_ARM_HAVE_TLS_REGISTER := true
-TARGET_BOARD_PLATFORM := mt6752
-TARGET_NO_BOOTLOADER := true
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_NO_FACTORYIMAGE := true
+ARCH_ARM_HAVE_TLS_REGISTER                    := true
+TARGET_BOARD_PLATFORM                         := mt6752
+TARGET_USERIMAGES_USE_EXT4                    := true
+TARGET_NO_FACTORYIMAGE                        := true
 
-TARGET_LDPRELOAD += libxlog.so
+TARGET_LDPRELOAD                              += libxlog.so
 
 # CPU
-TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
-TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 := 
-TARGET_CPU_VARIANT := generic
-TARGET_CPU_SMP := true
+TARGET_ARCH                                   := arm64
+TARGET_ARCH_VARIANT                           := armv8-a
+TARGET_CPU_ABI                                := arm64-v8a
+TARGET_CPU_ABI2                               := 
+TARGET_CPU_VARIANT                            := generic
+TARGET_CPU_SMP                                := true
+TARGET_2ND_ARCH                               := arm
+TARGET_2ND_ARCH_VARIANT                       := armv7-a-neon
+TARGET_2ND_CPU_ABI                            := armeabi-v7a
+TARGET_2ND_CPU_ABI2                           := armeabi
+TARGET_2ND_CPU_VARIANT                        := cortex-a53
+TARGET_BOARD_SUFFIX                           := _64
+TARGET_USES_64_BIT_BINDER                     := true
 
-TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv7-a-neon
-TARGET_2ND_CPU_ABI := armeabi-v7a
-TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a53
-TARGET_BOARD_SUFFIX := _64
-TARGET_USES_64_BIT_BINDER := true
+TARGET_CPU_CORTEX_A53                         := true
 
-TARGET_CPU_CORTEX_A53 := true
-
-TARGET_BOOTLOADER_BOARD_NAME := mt6752
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME                  := mt6752
+TARGET_NO_BOOTLOADER                          := true
 
 # MTK Hardware
-BOARD_HAS_MTK_HARDWARE := true
-MTK_HARDWARE := true
-BOARD_USES_LEGACY_MTK_AV_BLOB := true
-COMMON_GLOBAL_CFLAGS += -DMTK_HARDWARE -DADD_LEGACY_ACQUIRE_BUFFER_SYMBOL
-COMMON_GLOBAL_CPPFLAGS += -DMTK_HARDWARE
+BOARD_HAS_MTK_HARDWARE                        := true
+MTK_HARDWARE                                  := true
+BOARD_USES_LEGACY_MTK_AV_BLOB                 := true
+COMMON_GLOBAL_CFLAGS                          += -DMTK_HARDWARE -DADD_LEGACY_ACQUIRE_BUFFER_SYMBOL
+COMMON_GLOBAL_CPPFLAGS                        += -DMTK_HARDWARE
 
-BOARD_CONNECTIVITY_VENDOR := MediaTek
-BOARD_USES_MTK_AUDIO := true
+BOARD_CONNECTIVITY_VENDOR                     := MediaTek
+BOARD_USES_MTK_AUDIO                          := true
 
 # Kernel
-TARGET_USES_64_BIT_BINDER := true
-TARGET_IS_64_BIT := true
-BOARD_KERNEL_CMDLINE := 
-BOARD_KERNEL_BASE := 
-BOARD_KERNEL_PAGESIZE := 
-BOARD_MKBOOTIMG_ARGS := 
-TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/kernel
-BOARD_CUSTOM_BOOTIMG := true
+TARGET_USES_64_BIT_BINDER                     := true
+TARGET_IS_64_BIT                              := true
+BOARD_KERNEL_CMDLINE                          := 
+BOARD_KERNEL_BASE                             := 
+BOARD_KERNEL_PAGESIZE                         := 
+BOARD_MKBOOTIMG_ARGS                          := 
+TARGET_PREBUILT_KERNEL                        := $(LOCAL_PATH)/prebuilt/kernel
+BOARD_CUSTOM_BOOTIMG                          := true
 
 # MKImage
-TARGET_MKIMAGE := $(LOCAL_PATH)/mkimage
-TARGET_USE_BUILT_BOOTIMAGE := true
+TARGET_MKIMAGE                                := $(LOCAL_PATH)/mkimage
+TARGET_USE_BUILT_BOOTIMAGE                    := true
 
-TARGET_KMODULES := true
+TARGET_KMODULES                               := true
 # Disable memcpy opt (for audio libraries)
-TARGET_CPU_MEMCPY_OPT_DISABLE := true
+TARGET_CPU_MEMCPY_OPT_DISABLE                 := true
 
 # Display
-USE_OPENGL_RENDERER := true
-BOARD_EGL_CFG := $(LOCAL_PATH)/configs/egl.cfg
-TARGET_SCREEN_HEIGHT := 1280
-TARGET_SCREEN_WIDTH := 720
-BOARD_EGL_WORKAROUND_BUG_10194508 := true
+USE_OPENGL_RENDERER                           := true
+BOARD_EGL_CFG                                 := $(LOCAL_PATH)/configs/egl.cfg
+TARGET_SCREEN_HEIGHT                          := 1280
+TARGET_SCREEN_WIDTH                           := 720
+BOARD_EGL_WORKAROUND_BUG_10194508             := true
 
 # Flags
-TARGET_GLOBAL_CFLAGS   += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
-COMMON_GLOBAL_CFLAGS += -DDISABLE_HW_ID_MATCH_CHECK
-TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
+TARGET_GLOBAL_CFLAGS                          += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS                        += -mfpu=neon -mfloat-abi=softfp
+COMMON_GLOBAL_CFLAGS                          += -DNO_SECURE_DISCARD
+COMMON_GLOBAL_CFLAGS                          += -DDISABLE_HW_ID_MATCH_CHECK
+TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK         := true
 
 # LightHAL
-TARGET_PROVIDES_LIBLIGHT := true
+TARGET_PROVIDES_LIBLIGHT                      := true
 
 # Enable Minikin text layout engine (will be the default soon)
-USE_MINIKIN := true
+USE_MINIKIN                                   := true
 
 # Charger
-BOARD_CHARGER_SHOW_PERCENTAGE := true
+BOARD_CHARGER_SHOW_PERCENTAGE                 := true
 
 # Fonts
-EXTENDED_FONT_FOOTPRINT := true
+EXTENDED_FONT_FOOTPRINT                       := true
 
-TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
-
-# PREBUILT_Chromium
-#PRODUCT_PREBUILT_WEBVIEWCHROMIUM := yes
+# System.prop
+TARGET_SYSTEM_PROP                            := $(LOCAL_PATH)/system.prop
 
 # Bluetooth
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_MTK := true
-BOARD_BLUETOOTH_DOES_NOT_USE_RFKILL := true
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
+BOARD_HAVE_BLUETOOTH                          := true
+BOARD_HAVE_BLUETOOTH_MTK                      := true
+BOARD_BLUETOOTH_DOES_NOT_USE_RFKILL           := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR   := $(LOCAL_PATH)/bluetooth
 
 # GPS
-BOARD_GPS_LIBRARIES :=true
-BOARD_CONNECTIVITY_MODULE := CONSYS_MT6752 
-BOARD_MEDIATEK_USES_GPS := true
+BOARD_GPS_LIBRARIES                           :=true
+BOARD_CONNECTIVITY_MODULE                     := CONSYS_MT6752 
+BOARD_MEDIATEK_USES_GPS                       := true
 
 # FM
-MTK_FM_SUPPORT :=true
-MTK_FM_RX_SUPPORT :=true
+MTK_FM_SUPPORT                                :=true
+MTK_FM_RX_SUPPORT                             :=true
 
 # Consumerir
-MTK_IRTX_SUPPORT :=true
+MTK_IRTX_SUPPORT                              :=true
 
 # MTK_WLAN_SUPPORT
-WPA_SUPPLICANT_VERSION := VER_0_8_X
-BOARD_HOSTAPD_DRIVER := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_mt66xx
-BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_mt66xx
-WIFI_DRIVER_FW_PATH_PARAM := "/dev/wmtWifi"
-WIFI_DRIVER_FW_PATH_STA := STA
-WIFI_DRIVER_FW_PATH_AP := AP
-WIFI_DRIVER_FW_PATH_P2P := P2P
+WPA_SUPPLICANT_VERSION                        := VER_0_8_X
+BOARD_HOSTAPD_DRIVER                          := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB                     := lib_driver_cmd_mt66xx
+BOARD_WPA_SUPPLICANT_DRIVER                   := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB              := lib_driver_cmd_mt66xx
+WIFI_DRIVER_FW_PATH_PARAM                     := "/dev/wmtWifi"
+WIFI_DRIVER_FW_PATH_STA                       := STA
+WIFI_DRIVER_FW_PATH_AP                        := AP
+WIFI_DRIVER_FW_PATH_P2P                       := P2P
 
 # make_ext4fs requires numbers in dec format
-TARGET_USERIMAGES_USE_EXT4 := true
-BOARD_BOOTIMAGE_PARTITION_SIZE := 20971520
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 20971520
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1610612736
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 13474725888
-BOARD_FLASH_BLOCK_SIZE := 131072
+TARGET_USERIMAGES_USE_EXT4                    := true
+BOARD_BOOTIMAGE_PARTITION_SIZE                := 20971520
+BOARD_RECOVERYIMAGE_PARTITION_SIZE            := 20971520
+BOARD_SYSTEMIMAGE_PARTITION_SIZE              := 1610612736
+BOARD_USERDATAIMAGE_PARTITION_SIZE            := 13474725888
+BOARD_FLASH_BLOCK_SIZE                        := 131072
 
-BOARD_HAS_LARGE_FILESYSTEM := true
-TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_HAS_LARGE_FILESYSTEM                    := true
+TARGET_USERIMAGES_USE_EXT4                    := true
 
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun/file
+TARGET_USE_CUSTOM_LUN_FILE_PATH               := /sys/class/android_usb/android0/f_mass_storage/lun/file
 
-BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_RECOVERY_SWIPE := true
-BOARD_SUPPRESS_EMMC_WIPE := true
-BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/recovery.fstab
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
-TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_HAS_NO_SELECT_BUTTON                    := true
+BOARD_RECOVERY_SWIPE                          := true
+BOARD_SUPPRESS_EMMC_WIPE                      := true
+BOARD_USE_CUSTOM_RECOVERY_FONT                := \"roboto_15x24.h\"
+TARGET_RECOVERY_FSTAB                         := $(LOCAL_PATH)/rootdir/recovery.fstab
+TARGET_RECOVERY_PIXEL_FORMAT                  := "RGBA_8888"
+TARGET_USERIMAGES_USE_EXT4                    := true
 
 # SELinux
 BOARD_SEPOLICY_DIRS := \
@@ -326,4 +324,4 @@ BOARD_SEPOLICY_UNION += \
 	md_ctrl.te \
 	cmddumper.te \
 	tunman.te
-
+	
